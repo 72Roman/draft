1b49165dbc448b9c668dafafd1384f47b29dc08d
ryereshko <27romario@gmail.com>
2018-07-18 22:09:25 +0300

    added new functions.

--- Training3.js
+++ Training3.js
@@ -1,6 +1,33 @@
 // EloquentJavascript, data structure
 // Sum of a range
 
-function range (first, last) {
-	var arr = [];
+function range(first, last) {
+	let arr = [];
+	let cur = first;
+	for(let i = 0; i <= last - first; i++) {
+		arr[i] = cur;
+		cur++;
+	}
+	return arr; 
 }
+
+function sum(arr) {
+	let total = 0;
+	for(let i = 0; i < arr.length; i++) 
+		total += arr[i];
+	return total; 
+}
+
+
+function recRange(first, last) {
+	let arr = [];
+	function rec(first, last) {
+		if(last !== first) rec(first, last - 1);
+		arr.push(last);
+	}
+	rec(first, last);
+	return arr;
+}
+//console.log(range(0,0));
+console.log(sum(range(1,10)));
+//console.log(recRange(1,1));
