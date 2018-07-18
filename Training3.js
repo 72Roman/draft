9d47e600877eec0daa3a2d59f9a40b221c66b6cc
ryereshko <27romario@gmail.com>
2018-07-18 22:40:18 +0300

    added func which calculates sum of array.

--- Training3.js
+++ Training3.js
@@ -28,6 +28,15 @@ function recRange(first, last) {
 	rec(first, last);
 	return arr;
 }
+
+function recSum(arr) {
+	function rec(index) {
+		if(index === -1) return 0;
+		return arr[index] + rec(index - 1);
+	}
+	return rec(arr.length - 1);
+}
 //console.log(range(0,0));
-console.log(sum(range(1,10)));
+// console.log(sum(range(1,10)));
 //console.log(recRange(1,1));
+console.log(recSum(recRange(1,10)));
