dea74ac87bc6bfc999043758a744f49f2c1beff4
ryereshko <27romario@gmail.com>
2018-07-19 15:03:43 +0300

    new func reverseArrayInPlace.

--- Training4.js
+++ Training4.js
@@ -7,7 +7,16 @@ function reverseArray(arr) {
 	}
 	return copy;
 }
-console.log(reverseArray([1]));
+//console.log(reverseArray([1]));
 
-
-//function reverseArrayinPlace(arr)
+function reverseArrayinPlace(arr) {
+	let swap = 0;
+	for(let i = 0; i < arr.length / 2; i++) {
+		swap = arr[i];
+		arr[i] = arr[arr.length - i - 1];
+		arr[arr.length - i - 1] = swap;
+	}
+}
+//let array = [0,1,3,3,3,3,3,1,1,1,2,2,3,4];
+//reverseArrayinPlace(array);
+//console.log(array);
